a111acf3d47adf280db9c5e0fd14d40fa36c7d19
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-09-30 16:47:58 +0800

    Fix shasum: GNU coreutils uses sha256sum

--- scripts/get_session_colour.sh
+++ scripts/get_session_colour.sh
@@ -3,7 +3,14 @@ set -euo pipefail
 
 # https://superuser.com/questions/410017/how-do-i-know-current-tmux-session-name-by-running-tmux-command
 session_name=$(tmux display-message -p "#S")
-session_sha=$(echo "${session_name}" | shasum | cut -c1-6)
+if command -v shasum &> /dev/null; then
+  session_sha=$(echo "${session_name}" | shasum | cut -c1-6)
+elif command -v sha256sum &> /dev/null; then
+  session_sha=$(echo "${session_name}" | sha256sum | cut -c1-6)
+else
+  # fallback
+  session_sha=$(printf %x "$RANDOM")
+fi
 # https://stackoverflow.com/questions/13280131/hexadecimal-to-decimal-in-shell-script
 # https://stackoverflow.com/questions/428109/extract-substring-in-bash#428580
 r=$((16#${session_sha::2}))
